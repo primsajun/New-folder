4edea6117587d958c8fef262614433d345477df2
prim sajun <138086244+primsajun@users.noreply.github.com>
2025-05-01 19:01:04 +0530

    Update churn_prediction.js

--- churn_prediction.js
+++ churn_prediction.js
@@ -106,7 +106,7 @@ import * as dfd from 'danfojs-node';
 }  const normalizedTestData = testData.map(item => {
   return { normalizedTrainData, normalizedTestData, stats };
 }  const normalizedTestData = testData.map(item => {   // Calculate churn rate
-  const totalCustomers = data.length;
+  const totalCustomers = data.length
   const churnedCustomers = data.filter(customer => customer.churned === 1).length;
   const churnRate = churnedCustomers / totalCustomers;
   
