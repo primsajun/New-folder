bba26b22215186ed8345ebc91cb95d9f05d90f69
prim sajun <138086244+primsajun@users.noreply.github.com>
2025-05-11 20:05:20 +0530

    Update churn_prediction.js

--- churn_prediction.js
+++ churn_prediction.js
@@ -1,4 +1,4 @@
-noimport * as tf from '@tensorflow/tfjs-node';
+import * as tf from '@tensorflow/tfjs-node';
 import * as fs from 'fs';
 import * as csv from 'csv-parser';
 import { createReadStream } from 'fs';
