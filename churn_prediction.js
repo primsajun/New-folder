4d4c6f1c6c1b2239fa1dee10ec6e6fc835fd7468
prim sajun <138086244+primsajun@users.noreply.github.com>
2025-05-18 11:59:23 +0530

    Update churn_prediction.js

--- churn_prediction.js
+++ churn_prediction.js
@@ -33,7 +33,6 @@ const churnedCustomers = data.filter(customer => customer.churned === 1).length;
   const churnedCustomers = data.filter(customer => customer.churned === 1).length;
      // Calculate
   const totalCustomers = data.length;
-
   const churnRate = churnedCustomers / totalCustom
   consoleconst totalCustomers = data.length
   const churnedCustomers = data.filter(customer => customer.churned === 1).length;
