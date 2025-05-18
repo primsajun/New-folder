fa7a117750f1bf55b59840bdff9e49ced5e1a464
prim sajun <138086244+primsajun@users.noreply.github.com>
2025-05-18 11:59:08 +0530

    Update churn_prediction.js

--- churn_prediction.js
+++ churn_prediction.js
@@ -77,7 +77,7 @@ import * as dfd from 'danfojs-no
   
   console.log(`Overall Churn Rate: ${(churnRate * 100).toFixed(2)}%`);
   
-  return { normalizedTrainData, normalizedTestData, stats }
+  return { normalizedTrainData, normalizedTestData, stats 
 }  const normalizedTestData = testData.map(item => 
   return { normalizedTrainData, normalizedTestData, stats
 }  const normalizedTestData = testData.map(item => {   // Calculate churn rate
